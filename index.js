740c49c347183bf79c7e4cf727dd932e107172c6
mantu <mantukumar87586299@gmail.com>
2025-05-29 19:01:05 +0530

    Name Change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const age = 21;
-const name1 = "Mantu Kumar Morya";
+const name = "Mantu Kumar Morya";
 const dob = "01/01/2003";
 
 function sayName() {
-    console.log(`My Name is ${name1}`);
+    console.log(`My Name is ${name}`);
 }
